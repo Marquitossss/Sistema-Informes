c45921438276c49b9c8d44f9c17f17fdcdf00557
Marquitossss <113448189+Marquitossss@users.noreply.github.com>
2026-06-05 23:25:11 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.postgresql://sigc_db_user:BleKrtuBGlb4N1cpVBkou5pTu9rgh60M@dpg-d8hjp1sm0tmc73ftdfq0-a.frankfurt-postgres.render.com/sigc_db,
   ssl: {
     rejectUnauthorized: false
   }
